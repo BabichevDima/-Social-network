795976fe67de92ad249019a62373e337e8497858
BabichevDima <dima.babichev.1994@mail.ru>
2021-03-16 17:05:48 +0300

    refactor: refactor paginator

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Pagination from "react-bootstrap/Pagination";
 
 export const PaginationBasic = ({
   onPageChanged,
   totalUsersCount,
   pageSize,
+  portionSize = 6,
 }) => {
   const pagesCount = Math.ceil(totalUsersCount / pageSize);
+  const portionCount = Math.ceil(pagesCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionPageNumber = portionNumber * portionSize;
 
   const pages = [];
   for (let i = 1; i <= pagesCount; i++) {
@@ -16,28 +21,61 @@ export const PaginationBasic = ({
   return (
     <div>
       <Pagination>
-        <Pagination.First />
-        <Pagination.Prev />
-
-        <Pagination.Ellipsis />
-
-        {pages.map((number) => {
-          return (
-            <Pagination.Item
-              key={number}
-              onClick={(e) => {
-                onPageChanged(number);
+        {portionNumber > 2 ? (
+          <Pagination.First
+            onClick={() => {
+              setPortionNumber(portionNumber - 2);
+            }}
+          />
+        ) : null}
+        {portionNumber > 1 ? (
+          <>
+            <Pagination.Prev
+              onClick={() => {
+                setPortionNumber(portionNumber - 1);
               }}
-            >
-              {number}
-            </Pagination.Item>
-          );
-        })}
+            />
+            <Pagination.Ellipsis />
+          </>
+        ) : null}
 
-        <Pagination.Ellipsis />
+        {pages
+          .filter(
+            (number) =>
+              number >= leftPortionPageNumber &&
+              number <= rightPortionPageNumber
+          )
+          .map((number) => {
+            return (
+              <Pagination.Item
+                key={number}
+                onClick={(e) => {
+                  onPageChanged(number);
+                }}
+              >
+                {number}
+              </Pagination.Item>
+            );
+          })}
 
-        <Pagination.Next />
-        <Pagination.Last />
+        {portionCount > portionNumber ? (
+          <>
+            <Pagination.Ellipsis />
+            <Pagination.Next
+              onClick={() => {
+                setPortionNumber(portionNumber + 1);
+              }}
+            />
+          </>
+        ) : null}
+
+        {portionCount > portionNumber + 1 ? (
+          <Pagination.Last
+            onClick={() => {
+              setPortionNumber(portionNumber + 2);
+            }}
+          />
+        ) : null}
       </Pagination>
     </div>
   );
